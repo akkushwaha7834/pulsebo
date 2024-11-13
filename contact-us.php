855ba74e606bc9bd77d28325ac2fa9eca71edaed
akkushwaha7834 <akkushwaha018@gmail.com>
2024-11-13 17:53:29 +0530

    Contact Us page done

--- contact-us.php
+++ contact-us.php
@@ -1,3 +1,51 @@
 <?php include ('includes/header.php');?>
 
+<section class="contact-us" style="">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<h2 class="main-heading text-gradient-color">Let’s Connect</h2>
+				<p class="text-center">
+					Contact us with any questions or if you’d like more information about XAUBOT Group’s services.
+				</p>
+			</div>
+		</div>
+		<div class="row mt-5 contcat-info">
+			<div class="col-md-4">
+				<div class="image-box">
+					<i class="ri-mail-line"></i>
+					<h3 class="text-white">
+						Email Address
+					</h3>
+					<p>
+						<a class="text-white" href="mailto:info@pulsebot.com">info@pulsebot.com</a>
+					</p>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="image-box">
+					<i class="ri-smartphone-line"></i>
+					<h3 class="text-white">
+						Phone Number
+					</h3>
+					<p>
+						<a href="tel:+97142285717" class="text-white">+971 (4) 228 5717</a>
+					</p>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="image-box">
+					<i class="ri-questionnaire-line"></i>
+					<h3 class="text-white">
+						Clinet Questions
+					</h3>
+					<p>
+						WhastApp Support
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
 <?php include ('includes/footer.php');?>
