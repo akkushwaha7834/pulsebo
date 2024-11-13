f2f2a00f5d5f8e300ee7c3ad9ed02ee975d87744
akkushwaha7834 <akkushwaha018@gmail.com>
2024-11-13 17:19:00 +0530

    converted to .php and inlcuded header footer

---
+++ includes/footer.php
@@ -0,0 +1,68 @@
+ <footer>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2 col-md-4 col-6">
+                    <div class="footer_link">
+
+                        <h3>Company</h3>
+                        <ul>
+                            <li><a href="">About Us</a></li>
+                            <li><a href="">Contact Us</a></li>
+                            <li><a href="">Terms</a></li>
+                            <li><a href="">Policy</a></li>
+                            <li><a href="">Legal Notice</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-4 col-6">
+                    <div class="footer_link">
+
+                        <h3>Features</h3>
+                        <ul>
+                          <li><a href="">XAUBOT AI</a></li>
+                          <li><a href="">XAUBOT PRO</a></li>
+                          <li><a href="">Trading Academy</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-4 col-6">
+                    <div class="footer_link">
+
+                        <h3>Links</h3>
+                        <ul>
+                            <li><a href="">Affiliate</a></li>
+                            <li><a href="">Pricing</a></li>
+                            <li><a href="">Myfxbook</a></li>
+                            <li><a href="">TrustPilot</a></li>
+                            <li><a href="">Blog</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12  ">
+                    <div class="footer_disc">
+                        <img src="assets/img/light-logo.png" class="img-fluid" width="180px" alt="">
+                        <p>XAUBOT is the first AI trading tool in the world that is based on machine learning technology
+                            to achieve impressive profit margins while maintaining low drawdown rates.</p>
+                    </div>
+
+                </div>
+                <div class="footer_line"></div>
+                <p class="text-center pt-2">© 2023 - 2024 XAUBOT - All Rights Reserved.</p>
+            </div>
+        </div>
+
+
+
+
+    </footer>
+    <!-- Jquery CDN -->
+    <script src="https://xaubot.com/wp-content/themes/betheme/assets/js/jquery.js"></script>
+    <!-- bootstrap js -->
+    <script src="assets/js/bootstrap/bootstrap.bundle.min.js"></script>
+    <!-- chart js -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <script src="assets/js/script.js"></script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pulsebot</title>
+    <link rel="stylesheet" href="assets/css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/remixicon.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+
+<body>
+    <header id="mainhead">
+
+        <nav class="container" id="nav">
+            <div class="logo">
+                <a href="./">
+                    <img src="assets/img/light-logo.png" width="180px" alt="">
+                </a>
+            </div>
+            <div class="pages">
+                <ul>
+                    <li><a href="./">Home</a></li>
+                    <li class="dropdown"><a href="#">Products</a>
+                        <div class="dropdown_content">
+                            <a href="pulsebot-ai.php">Pulsebot AI</a>
+                            <a href="pulsebot-pro.php">Pulsebot PRO</a>
+                        </div>
+                    </li>
+                    <li><a href="pricing.php">Pricing</a></li>
+                    <li><a href="become-an-affiliate.php">Affiliate</a></li>
+                    <li><a href="blog.php">Blog</a></li>
+                    <li><a href="about-us.php">About</a></li>
+                    <li><a href="contact-us.php">Contact</a></li>
+                    <li><a href="pulsebot-webinar.php">Webinars</a></li>
+                </ul>
+            </div>
+            <div class="icons  ">
+                <a href="" class="user ">
+                    <i class="ri-user-line"></i>
+                </a>
+                <a href="" class="search ">
+                    <i class="ri-search-line"></i>
+                </a>
+                <a href="" class="cart ">
+                    <i class="ri-shopping-cart-line"></i>
+                </a>
+            </div>
+            <div class="mobile_menu">
+                <button class="btn btn-outline-light fw-bold" type="button" data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><i
+                        class="ri-menu-3-fill"></i></button>
+            </div>
+            <div class="offcanvas mobile_pages_list offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false"
+                tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title  text-light" id="offcanvasScrollingLabel"><div class="logo">
+                        <img src="assets/img/light-logo.png" width="180px" alt="">
+                    </div></h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <div class="pages-ofcanvas">
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Products</a></li>
+                            <li><a href="">Pricing</a></li>
+                            <li><a href="">Affiliate</a></li>
+                            <li><a href="">Blog</a></li>
+                            <li><a href="">About</a></li>
+                            <li><a href="">Contact</a></li>
+                            <li><a href="">Webinars</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+    </header>
