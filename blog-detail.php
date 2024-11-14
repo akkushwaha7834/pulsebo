8ea995436cfe75daf3c460a2832da91d1d7ce888
akkushwaha7834 <akkushwaha018@gmail.com>
2024-11-14 11:49:05 +0530

    blog-detail and blog page done

---
+++ blog-detail.php
@@ -0,0 +1,92 @@
+<?php include ('includes/header.php');?>
+
+<section class="blog-main-section">
+	<div class="container">
+		<div class="row text-center">
+			<div class="col-md-12">
+				<h2 class="text-gradient-color">
+					Pulsebot Blog Detail
+				</h2>
+				<p>
+					Achieve more with forex education hub.
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="blog-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8">
+				<div class="blog-details">
+					<h2>
+						Lorem Ipsum
+					</h2>
+					<p class="text-black">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+					<h3 class="text-gradient-color">
+						Lorem Ipsum
+					</h3>
+					<p class="text-black">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+					<h3 class="text-gradient-color">
+						Lorem Ipsum
+					</h3>
+					<p class="text-black">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+					<h3 class="text-gradient-color">
+						Lorem Ipsum
+					</h3>
+					<p class="text-black">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+					</p>
+					<h3 class="text-gradient-color">
+						Lorem Ipsum
+					</h3>
+					<p class="text-black">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+					</p>
+					<h3 class="text-gradient-color">
+						Lorem Ipsum
+					</h3>
+					<p class="text-black">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+					</p>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="sidebar-sticky">
+                    <div class="container">
+                        <div class="row gy-4">
+							<div class="col-md-12">
+                                <div class="card blog-card">
+                                    <a href="blog-detail.php">
+                                        <img src="assets/img/what-is-expert-advisor.jpg" class="card-img-top" alt="...">
+                                    </a>
+                                    <div class="card-body blog-content">
+                                        <a href="blog-detail.php">
+                                            <h5 class="card-title">Lorem Ipsum</h5>
+                                        </a>    
+                                        <hr style="border: 1px solid #ccc;">
+                                        <div class="meta_blog">
+                                            <span>
+                                                <i class="ri-calendar-line"></i>  11-11-2024</span>
+                                            <span>
+                                                <i class="ri-user-line"></i>  
+				                        Pulsebot</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include ('includes/footer.php');?>
--- blog.php
+++ blog.php
@@ -1,3 +1,57 @@
 <?php include ('includes/header.php');?>
 
+<section class="blog-main-section">
+	<div class="container">
+		<div class="row text-center">
+			<div class="col-md-12">
+				<h2 class="text-gradient-color">
+					Pulsebot Blog
+				</h2>
+				<p>
+					Achieve more with forex education hub.
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="blog-section">
+	<div class="container">
+		<div class="row g-4">
+			<?php
+				$repeatCount = 6;
+				for ($i = 0; $i < $repeatCount; $i++) {
+				    ?>
+				    <div class="col-lg-4 col-md-6">
+				        <div class="card blog-card">
+				            <a href="blog-detail.php">
+				                <img src="assets/img/what-is-expert-advisor.jpg" class="card-img-top" alt="...">
+				            </a>
+				            <div class="card-body blog-content">
+				                <a href="blog-detail.php">
+				                    <h5 class="card-title">
+				                        How to Customize Your Brokerage Platform to Stand Out in a Crowded Market with YaPrime
+				                    </h5>
+				                </a>
+				                <hr style="border: 1px solid #fff;">
+				                <div class="meta_blog">
+				                    <span>
+				                        <i class="ri-calendar-line"></i> 
+				                        11-11-2024            
+				                    </span>
+				                    <span>
+				                        <i class="ri-user-line"></i>  
+				                        Pulsebot
+				                    </span>
+				                </div>
+				            </div>
+				        </div>
+				    </div>
+			    <?php
+				}
+			?>
+		</div>
+	</div>
+</section>
+
 <?php include ('includes/footer.php');?>
