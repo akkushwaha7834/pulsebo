4772a6398a3f2fe3686201efc5cfb7f8f139ac68
akkushwaha7834 <akkushwaha018@gmail.com>
2024-11-14 10:46:45 +0530

    Affiliate Page done

--- become-an-affiliate.php
+++ become-an-affiliate.php
@@ -1,3 +1,41 @@
 <?php include ('includes/header.php');?>
 
+<section class="affiliate-banner" style="background: url(assets/img/galaxy-night.webp);">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-md-8 text-center">
+				<h2 class="text-gradient-color">Pulsebot Affiliate Program</h2>
+				<p>
+					With Pulsebot Affiliate Program you get money when people from your network become XAUBOT Customers. You get 25% commission. A win-win!
+				</p>
+			</div>
+			<div class="col-md-4 affiliate-cta-main">
+				<div class="affiliate-cta text-center">
+					<i class="ri-shake-hands-line text-gradient-color"></i>
+					<h3 class="text-gradient-color" style="font-weight: 700;">Affiliate Program</h3>
+					<p class="text-gradient-color">On every new paid XAUBOT account you refer</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="how-affiliate">
+	<div class="container">
+		<div class="row text-center align-items-center">
+			<div class="col-md-6">
+				<h2 class="text-gradient-color">
+					How affiliate marketing works?
+				</h2>
+				<p class="text-black">
+					Boost your earnings by sharing your unique link with new customers. Receive a generous 25% commission on any new transactions they make. Start referring today and enjoy the rewards!
+				</p>
+			</div>
+			<div class="col-md-6">
+				<img src="assets/img/affiliate.png" class="img-fluid" width="70%">
+			</div>
+		</div>
+	</div>
+</section>
+
 <?php include ('includes/footer.php');?>
