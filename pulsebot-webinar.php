91847e022275a9f53d159b10f890f99c13759c0a
akkushwaha7834 <akkushwaha018@gmail.com>
2024-11-14 14:48:48 +0530

    Webinar Page done

--- pulsebot-webinar.php
+++ pulsebot-webinar.php
@@ -1,3 +1,211 @@
 <?php include ('includes/header.php');?>
 
+<section class="webinar-main">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-md-6 text-center">
+				<h4 class="text-white">
+					FREE Special Training Webinar
+				</h4>
+				<h1 class="text-white webinar-heading">
+					3 Forex Strategies to Pass the Prop Challenges
+				</h1>
+				<h5 class="text-white">
+					11 September 2024, Zoom Platform
+				</h5>
+			</div>
+			<div class="col-md-6">
+				<div class="youtube-embed-section">
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/D0UnqGm_miA?si=sYPd7Bk-TH0j6oh9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="strategies">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<h3 class="strategies-head">
+					Top 3 Strategies for <span>Forex Trading</span> in 2024
+				</h3>
+				<h4>
+					What You’ll Discover
+				</h4>
+			</div>
+		</div>
+		<div class="row g-4 mt-3">
+			<div class="col-md-6">
+				<div class="image-box-webinar text-white">
+					<i class="ri-bar-chart-2-line"></i>
+					<h3 class="">
+						What is the Top Forex Strategies
+					</h3>
+					<p class="text-white">
+						We’ll guide you through three powerful trading strategies from scratch. Whether you're looking for short-term gains or long-term growth, these strategies are designed to fit your trading style.
+					</p>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="image-box-webinar text-white">
+					<i class="ri-bar-chart-2-line"></i>
+					<h3 class="">
+						Improve Your Results in the Forex
+					</h3>
+					<p class="text-white">
+						Learn how much you can realistically Increase your outcome in the Forex market and what factors contribute to your success. Understand the potential and set your financial goals.
+					</p>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="image-box-webinar text-white">
+					<i class="ri-bar-chart-2-line"></i>
+					<h3 class="">
+						What is the Best Currency Pairs
+					</h3>
+					<p class="text-white">
+						Find out which currency pairs are expected to perform well in 2024 and the ideal time frames to trade them. Get insights into market trends and make informed decisions.
+					</p>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="image-box-webinar text-white">
+					<i class="ri-bar-chart-2-line"></i>
+					<h3 class="">
+						What is the Best Time Frames
+					</h3>
+					<p class="text-white">
+						The most effective time frames for your trading strategies and learn how to align them with market conditions. Gain practical tips and insights to optimize your trading approach .
+					</p>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="image-box-webinar text-white">
+					<i class="ri-bar-chart-2-line"></i>
+					<h3 class="">
+						How to Create Your Daily Trading Journal
+					</h3>
+					<p class="text-white">
+						Learn how to document your trades, analyze your performance, and make continuous improvements. A trading journal is essential for tracking your progress and refining your strategies.
+					</p>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="image-box-webinar text-white">
+					<i class="ri-bar-chart-2-line"></i>
+					<h3 class="">
+						How to Backtest and Analyze
+					</h3>
+					<p class="text-white">
+						We’ll guide you through 3 powerful trading strategies from scratch. Whether you're looking for short-term gains or long-term growth, these strategies are designed to fit your trading style.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="register-now strategies">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<h3 class="strategies-head">
+					Got Questions About <span>Trading</span>
+				</h3>
+				<h4>
+					Get Answers in this FREE Webinar
+				</h4>
+				<div class="cta-btn mt-5">
+					<a href="#">Register Now</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="faq-section strategies">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-md-12 text-center">
+				<h3 class="strategies-head">
+					Frequently Asked <span>Questions</span>
+				</h3>
+				<p>
+					If you have any other queries, feel free to reach out to us via email or WhatsApp, and we'll gladly assist you.
+				</p>
+			</div>
+			<div class="col-md-8">
+				<div class="accordion faq-accordion mt-5 mb-5" id="accordionExample">
+                        <div class="accordion-item">
+                            <h3 class="accordion-header" id="headingOne">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                                    What currency pairs can the bot trade?
+                                </button>
+                            </h3>
+                            <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
+                                data-bs-parent="#accordionExample" style="">
+                                <div class="accordion-body">
+                                    XAUBOT Works with ALL forex currency pairs
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h3 class="accordion-header" id="headingTwo">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                    What brokers does the robot support and which broker do you recommend?
+                                </button>
+                            </h3>
+                            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                                data-bs-parent="#accordionExample" style="">
+                                <div class="accordion-body">
+                                    While we don't recommend any brokers to our customers, here are brokers that have
+                                    been tested by us and our customers have had positive results with: IC Markets,
+                                    Pepperstone, Tickmill, RannForex, XM, FIBO Group, Coinexx, RoboForex, ThinkMarkets,
+                                    Vantage, Oanda, MexAtlantic, Windsor, Exness, FXCM, 8Cap.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h3 class="accordion-header" id="headingThree">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                    Is it possible to change the settings of the bot?
+                                </button>
+                            </h3>
+
+                            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+                                data-bs-parent="#accordionExample" style="">
+                                <div class="accordion-body">
+                                    Yes, the bot can be adjusted in 3 modes: low, medium and high, in each of which the
+                                    amount
+                                    of
+                                    monthly profit and drawdown is different.
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="accordion-item">
+                            <h3 class="accordion-header" id="heading4">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#collapse4" aria-expanded="false" aria-controls="collapse4">
+                                    What is the monthly profit?
+                                </button>
+                            </h3>
+                            <div id="collapse4" class="accordion-collapse collapse" aria-labelledby="heading4"
+                                data-bs-parent="#accordionExample" style="">
+                                <div class="accordion-body">
+
+                                    It depends on the market conditions, but around 2% to 10% based on your desire risk.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+			</div>
+		</div>
+	</div>
+</section>
+
 <?php include ('includes/footer.php');?>
